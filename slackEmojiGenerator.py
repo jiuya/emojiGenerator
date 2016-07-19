40d75772e736e734759157ec98be075b7850339e
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2016-07-19 21:43:59 +0900

    コマンドラインからの色指定の追加

--- slackEmojiGenerator.py
+++ slackEmojiGenerator.py
@@ -10,13 +10,18 @@ if __name__ == '__main__':
     if argc == 1:
         exit()
     for num in range(1,argc):
-        text.append(argvs[num])
+        if(argvs[num][0] == '#'):
+            r = int(argvs[num][1]+argvs[num][2],16)
+            g = int(argvs[num][3]+argvs[num][4],16)
+            b = int(argvs[num][5]+argvs[num][6],16)
+        else:
+            text.append(argvs[num])
 
-    emoji = String2emoji(text,fontFile)
+    emoji = String2emoji(text,fontFile,(r,g,b))
 
     img = emoji.getEmoji()
     saveFile = 'emoji_'
-    for i in range(0,argc-1):
+    for i in range(0,len(text)):
         saveFile += text[i].decode('mbcs')
     saveFile += '.png'
     img.save(saveFile)

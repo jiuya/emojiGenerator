7e01cc4f96dd885feefd5f4d12625f9053483d97
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2016-07-28 21:34:08 +0900

    無駄に画像生成した部分を改善

--- String2emoji.py
+++ String2emoji.py
@@ -20,54 +20,45 @@ class String2emoji(object):
     def setFontColor(self,color):
         self.fontColor = color
     def cutEffectiveRange(self,text,wMax,hMax):
-        for i in range(hMax/2,hMax*2):
+        for i in range(hMax,hMax*2):
             font = self.getFont(i)
             w, h = font.getsize(text)
-            x0 = self.stringOverBorderX(text,font,h)
-            y0 = self.stringOverBorderY(text,font,w)
-            x1 = self.stringUnderBorderX(text,font,x0,w+2)
-            y1 = self.stringUnderBorderY(text,font,y0,h)
-            if (x1 >= wMax-1) or (y1 >= hMax-1) :
-                return (i-1,x0,y0,x1,y1);
-    def stringOverBorderX(self,text,font,h):
-        for x in range(0,-128,-1):
-            img = Image.new("RGBA",(128,h),self.backColor)
+            img = Image.new("RGBA",(w,h),self.backColor)
             draw = ImageDraw.Draw(img)
-            draw.text((x,0), text.decode('mbcs'), fill=self.fontColor, font=font)
+            draw.text((0,0), text.decode('mbcs'), fill=self.fontColor, font=font)
+            x0 = self.stringOverBorderX(img,text,font,h)
+            y0 = self.stringOverBorderY(img,text,font,w)
+            x1 = self.stringUnderBorderX(img,text,font,w,h) + x0
+            y1 = self.stringUnderBorderY(img,text,font,h,w) + y0
+            if ((x1 >= wMax-5) and (x1 < wMax)) or ((y1 >= hMax-5) and (y1 < hMax)) :
+                return (i,x0,y0,x1,y1);
+    def stringOverBorderX(self,img,text,font,h):
+        for x in range(0,-127,-1):
             limitFlag = 0
-            for i in range(0,h):
-                color = img.getpixel((0,i))
+            for i in range(0,h-1):
+                color = img.getpixel((-x,i))
                 if color != self.backColor:
                     limitFlag = 1;
             if limitFlag > 0:
                 return x
-    def stringOverBorderY(self,text,font,w):
-        for y in range(0,-128,-1):
-            img = Image.new("RGBA",(w,128),self.backColor)
-            draw = ImageDraw.Draw(img)
-            draw.text((0,y), text.decode('mbcs'), fill=self.fontColor, font=font)
+    def stringOverBorderY(self,img,text,font,w):
+        for y in range(0,-127,-1):
             limitFlag = 0
-            for i in range(0,w):
-                color = img.getpixel((i,0))
+            for i in range(0,w-1,2):
+                color = img.getpixel((i,-y))
                 if color != self.backColor:
                     limitFlag = 1;
             if limitFlag > 0:
                 return y
-    def stringUnderBorderX(self,text,font,x0,w):
-        img = Image.new("RGBA",(w,128),self.backColor)
-        draw = ImageDraw.Draw(img)
-        draw.text((x0,0), text.decode('mbcs'), fill=self.fontColor, font=font)
-        for cx in range(w-1,0,-1):
-            for cy in range(0,128):
+    def stringUnderBorderX(self,img,text,font,w,h):
+        for cx in range(w-1,0,-2):
+            for cy in range(0,h-1):
                 color = img.getpixel((cx,cy))
                 if color != self.backColor:
                     return cx;
-    def stringUnderBorderY(self,text,font,y0,h):
-        img = Image.new("RGBA",(128,128),self.backColor)
-        draw = ImageDraw.Draw(img)
-        draw.text((0,y0), text.decode('mbcs'), self.fontColor, font=font)
-        for cy in range(127,0,-1):
-            for cx in range(0,128):
+    def stringUnderBorderY(self,img,text,font,h,w):
+        for cy in range(h-1,0,-1):
+            for cx in range(0,w-1,2):
                 color = img.getpixel((cx,cy))
                 if color != self.backColor:
                     return cy;
